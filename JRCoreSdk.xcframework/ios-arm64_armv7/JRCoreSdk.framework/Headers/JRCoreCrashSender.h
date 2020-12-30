12bb6c040aa7f41d2c2882c9252b44e457587476
chunta <imchuntachen@gmail.com>
2020-12-30 17:10:34 +0800

    update

---
+++ JRCoreSdk.xcframework/ios-arm64_armv7/JRCoreSdk.framework/Headers/JRCoreCrashSender.h
@@ -0,0 +1,16 @@
+//
+//  JRCoreCrashSender.h
+//  JRCoreSdk
+//
+//  Created by chunta on 2020/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JRCoreCrashSender : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
