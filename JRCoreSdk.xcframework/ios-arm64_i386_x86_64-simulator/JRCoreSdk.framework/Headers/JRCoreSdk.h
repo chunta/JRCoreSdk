0b61d06472b9f0b6d9c91c29364720ac78911a4a
chunta <imchuntachen@gmail.com>
2020-12-30 17:04:28 +0800

    fix

--- JRCoreSdk.xcframework/ios-arm64_i386_x86_64-simulator/JRCoreSdk.framework/Headers/JRCoreSdk.h
+++ JRCoreSdk.xcframework/ios-arm64_i386_x86_64-simulator/JRCoreSdk.framework/Headers/JRCoreSdk.h
@@ -14,5 +14,5 @@ FOUNDATION_EXPORT double JRCoreSdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char JRCoreSdkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JRCoreSdk/PublicHeader.h>
-#import <JRCoreSdk/JRCoreLogger.h>
+
 
